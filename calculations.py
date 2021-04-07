4e27db801324250dd9291c56816a955e8ceda703
dazeeeed <dazeeeed@gmail.com>
2021-04-07 16:46:16 +0200

    time dependent psi animation

--- calculations.py
+++ calculations.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 def calc_pm(m, n_x, step_x):
-    '''Calculation of p_m coefficients, where m-mass, n_x- lentgh of x, step_x- dx''' 
+    '''
+    Calculation of p_m coefficients, where m-mass, n_x- lentgh of x, step_x- dx.
+    p_m = 2*pi*f_m, where f_m=k (notation in articles)-frequencies of each 
+    Fourier transform component.
+    ''' 
     return np.array([2*np.pi*m/(n_x*step_x) if m <= n_x/2 \
         else 2*np.pi*(m-n_x)/(n_x*step_x) \
         for m in range(n_x)])
@@ -12,4 +16,22 @@ for m in range(n_x):
         p_m.append(2*np.pi*m/(n_x*step_x))
     else:
         p_m.append(2*np.pi*(m-n_x)/(n_x*step_x))
+'''
+
+'''
+#######################################################
+# 3D Ploting
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+ax.plot3D(x,f(x))
+for i in range(n):
+    ax.plot3D(x, np.real(psi[i]), np.imag(psi[i]), label="{} pochodna".format(i+1))
+
+ax.set_xlabel('x')
+ax.set_ylabel("Real")
+ax.set_zlabel("Imag")
+
+plt.show()
 '''
--- derivative_fft.py
+++ derivative_fft.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d #only for 3d plots
+import matplotlib.animation as animation
+#from mpl_toolkits import mplot3d #only for 3d plots
 from calculations import calc_pm
 
-def f(x,t):
-    # return np.exp(-a*x*x)
-    return np.exp(-a*(x-t)**2)
+def f(x):
+    return np.exp(-a*x*x)
+    #return np.exp(-a*(x-t)**2)
 
 def NL(V):
     '''Nonlinear exponential, where V is potential.'''
@@ -15,17 +16,21 @@ def DS(k):
     '''Dispersive exponential'''
     return np.exp(1j*dt*k**2 / (2*m))
 
+def eq1221(x,t, step_x=0.01, m=1, h=1):
+    '''Equation 12.21 from some ultra-wise physics book.'''
+    return (2*np.pi*(step_x**2+h**2*t**2/(4*m**2*step_x**2)))**(-1/2) * \
+        np.exp(-x**2/(2*(step_x**2+h**2*t**2/(4*m**2*step_x**2))))
+
 a = 0.5
+h = 1
 x_marg, step_x = 10, 0.01
 V, m = 0, 1
-dt = 0.1
+dt = 0.05
 x = np.arange(-x_marg, x_marg, step_x)
-t_arr = np.arange(0, 2, dt)
-
 
 #######################################################
 # p_m COEFFICIENTS
-p_m = calc_pm(m, len(x), step_x)
+p_m = calc_pm(m, len(x), step_x) #/ (2*np.pi)
 
 '''
 try:
@@ -35,50 +40,58 @@ except:
     exit()
 '''
 n = 2
+
 #######################################################
 # FOURIER TRANSFORM
 psi = np.empty((n+1,len(x)), dtype=np.complex64)
-#each row corresponds to (i+1)-th derivative, eg psi[0] = psi(x), psi[1] = dpsi(x)/dx
+#each row corresponds to i-th derivative, eg psi[0] = psi(x), psi[1] = dpsi(x)/dx
 
-psi[0] = np.fft.fft(NL(V)*f(x,0))
-
-for i in range(1,n+1):
-    psi[i] = (1j*p_m)**i * psi[0] 
-    psi[i] = np.fft.ifft(DS(p_m) * psi[i]) 
-    # psi[i] = np.fft.ifft(psi[i]) 
-
-psi[0] = np.fft.ifft(DS(p_m) * psi[0])
+for iterations in range(1):
+    psi[0] = np.fft.fft(NL(V)*f(x))
+    # for i in range(1,n+1):
+        # #This loop is for calculation of derivatives
+        # psi[i] = (1j*p_m)**i * psi[0] 
+        # psi[i] = np.fft.ifft(DS(p_m) * psi[i]) 
+    psi[0] =  np.fft.ifft(DS(p_m) *psi[0])
 
 #######################################################
 # PLOT CREATION
 fig = plt.figure()
 ax = plt.axes()
 
-ax.plot(x,np.real(psi[0]), label='f(x)')
-for i in range(1,n+1):
-    ax.plot(x, np.real(psi[i]), label=f"$d^{i}/dx^{i} f(x)$", linewidth=5*np.random.rand(1))
+# ax.plot(x, np.abs(psi[0]), label='f(x)')
+# for i in range(1,n+1):
+#     ax.plot(x, np.abs(psi[i]), label=f"$d^{i}/dx^{i} f(x)$", linewidth=5*np.random.rand(1))
 
 # ANALYTIC DERIVATIVE FORMULAS
 # ax.plot(x, -2*a*x*f(x), linestyle='-.', linewidth=3, label="Analytic f'(x)")
 # ax.plot(x, 2*a*f(x)*(2*a*x*x-1), linestyle='-.', linewidth=3, label="Analytic f''(x)")
 
-plt.legend()
+# y = eq1221(x,dt)
+y = psi[0]
+line, = ax.plot(0,0)
+ax.set_xlim(-x_marg,x_marg)
+ax.set_ylim(-1,1)
+
+def animation_frame(i):
+    global y, dt
+    if not pause:
+        y = np.fft.fft(NL(V) * y)
+        y = np.fft.ifft(DS(p_m) * y)
+        line.set_xdata(x)
+        line.set_ydata(np.abs(y))
+        print('Time: {} s'.format(np.round(i*dt,2)))
+    return line,
+
+pause = False
+def onClick(event):
+    global pause
+    pause ^= True
+fig.canvas.mpl_connect('button_press_event', onClick)
+
+anim = animation.FuncAnimation(fig, func=animation_frame, frames=201, interval=1, repeat=False)
+#uncomment below to save animation, needs imagemagick installed
+#anim.save("/tmp/animation.gif", writer="imagemagick", fps=30)
+
+#plt.legend()
 plt.show()
-
-#######################################################
-# 3D Ploting
-
-'''
-fig = plt.figure()
-ax = plt.axes(projection="3d")
-
-ax.plot3D(x,f(x))
-for i in range(n):
-    ax.plot3D(x, np.real(psi[i]), np.imag(psi[i]), label="{} pochodna".format(i+1))
-
-ax.set_xlabel('x')
-ax.set_ylabel("Real")
-ax.set_zlabel("Imag")
-
-plt.show()
-'''
