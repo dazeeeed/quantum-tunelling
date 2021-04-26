7f23a24735f88f7b02eca7c83c7b1af707603f82
Lukaszz99 <l.saw99@outlook.com>
2021-04-26 18:20:48 +0200

    final

---
+++ test_files/calculations.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def calc_pm(m, n_x, step_x):
+    '''Calculation of p_m coefficients, where m-mass, n_x- lentgh of x, step_x- dx.
+    p_m = 2*pi*f_m, where f_m=k (notation in articles)-frequencies of each 
+    Fourier transform component.''' 
+    
+    return np.array([2*np.pi*m/(n_x*step_x) if m <= n_x/2 \
+        else 2*np.pi*(m-n_x)/(n_x*step_x) \
+        for m in range(n_x)])
+
+''' ^^^ is equivalent with:
+for m in range(n_x):
+    if m <= n_x/2:
+        p_m.append(2*np.pi*m/(n_x*step_x))
+    else:
+        p_m.append(2*np.pi*(m-n_x)/(n_x*step_x))
+'''
+
+def V_barrier(x, x_0, value=0, width=0):
+    '''Potential barrier array creation. x_0 is a position of the barrier. 
+    Value is a height of this barrier. Width is a width of the barrier.'''
+    barrier = np.empty(len(x))
+    for ix in range(len(x)):
+        if x[ix] >= x_0 and x[ix] <= x_0 + width:
+            barrier[ix] = value
+        elif x[ix] >= x_0+4 and x[ix] <= x_0+4 + width:
+            barrier[ix] = value + 1
+        elif x[ix] >= x_0+10 and x[ix] <= x_0+10 + width:
+            barrier[ix] = value + 2
+        else:
+            barrier[ix] = 0
+
+    return barrier
+
+
+'''
+#######################################################
+# 3D Ploting
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+ax.plot3D(x,f(x))
+for i in range(n):
+    ax.plot3D(x, np.real(psi[i]), np.imag(psi[i]), label="{} pochodna".format(i+1))
+
+ax.set_xlabel('x')
+ax.set_ylabel("Real")
+ax.set_zlabel("Imag")
+
+plt.show()
+'''
+
+'''
+# ax.plot(x, np.abs(psi[0]), label='f(x)')
+# for i in range(1,n+1):
+#     ax.plot(x, np.abs(psi[i]), label=f"$d^{i}/dx^{i} f(x)$", linewidth=5*np.random.rand(1))
+
+# ANALYTIC DERIVATIVE FORMULAS
+# ax.plot(x, -2*a*x*f(x), linestyle='-.', linewidth=3, label="Analytic f'(x)")
+# ax.plot(x, 2*a*f(x)*(2*a*x*x-1), linestyle='-.', linewidth=3, label="Analytic f''(x)")
+'''
---
+++ test_files/derivative_fft.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#from mpl_toolkits import mplot3d #only for 3d plots
+from calculations import calc_pm, V_barrier
+
+def f(x):
+    return np.exp(-a*x*x)
+    #return np.exp(-a*(x-t)**2)
+
+def NL(V,dt=0.05):
+    '''Nonlinear exponential, where V is potential.'''
+    return np.exp(-1j*dt*V)
+
+def DS(k, dt=0.05, m=1):
+    '''Dispersive exponential'''
+    return np.exp(1j*dt*k**2 / (2*m))
+
+def eq1221(x,t, step_x=0.01, m=1, h=1):
+    '''Equation 12.21 from some ultra-wise physics book.'''
+    return (2*np.pi*(step_x**2+h**2*t**2/(4*m**2*step_x**2)))**(-1/2) * \
+        np.exp(-x**2/(2*(step_x**2+h**2*t**2/(4*m**2*step_x**2))))
+
+a = 0.5
+h = 1
+x_marg, step_x = 100, 0.01
+m = 1
+dt = 0.05
+x = np.arange(-x_marg, x_marg, step_x)
+V = V_barrier(x, 1, 1.5, 0.5)
+
+
+#######################################################
+# p_m COEFFICIENTS
+p_m = calc_pm(m, len(x), step_x) #/(2*np.pi)
+
+n = 2 # how many derivatives to count - NOT USED
+
+#######################################################
+# FOURIER TRANSFORM
+psi = np.empty((n+1,len(x)), dtype=np.complex64)
+#each row corresponds to i-th derivative, eg psi[0] = psi(x), psi[1] = dpsi(x)/dx
+
+for iterations in range(1):
+    psi[0] = np.fft.fft(NL(V)*f(x))
+    # for i in range(1,n+1):
+        # #This loop is for calculation of derivatives
+        # psi[i] = (1j*p_m)**i * psi[0] 
+        # psi[i] = np.fft.ifft(DS(p_m) * psi[i]) 
+    psi[0] =  np.fft.ifft(DS(p_m) *psi[0])
+
+#######################################################
+# PLOT CREATION
+fig = plt.figure()
+ax = plt.axes()
+
+y = eq1221(x,0.01)
+#y = psi[0]
+line, = ax.plot(0,0)
+ax.set_xlim(-0.5*x_marg,0.5*x_marg)
+ax.set_ylim(-0.01,4)
+ax.plot(x, V)
+time = ax.text(0.35, 0.9, "Time: 0 s",
+        verticalalignment='bottom', horizontalalignment='right',
+        transform=ax.transAxes,
+        color='green', fontsize=15)
+
+def animation_frame(i):
+    global y, dt, p_m
+    if not pause:
+        time.set_text("Time: {} s".format(np.round(i*dt,2)))
+        y = np.fft.fft(NL(V,dt) * y)
+
+        #y = np.fft.ifft(DS(p_m,dt) * y)
+        y = np.fft.ifft(DS(0.25*p_m - 60*dt, dt) * y)
+        
+        line.set_xdata(x)
+        line.set_ydata(np.abs(y))
+    return line,
+
+pause = False
+def onClick(event):
+    global pause
+    pause ^= True
+
+fig.canvas.mpl_connect('button_press_event', onClick)
+
+anim = animation.FuncAnimation(fig, func=animation_frame, frames=3*200, interval=10, repeat=False)
+
+#plt.legend()
+plt.show()
+
+
